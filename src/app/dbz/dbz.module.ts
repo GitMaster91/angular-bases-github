a0966256045020eea55207e7e4b54e95aacbcf6f
luis.villar <luis.villar@next-technologies.com.mx>
2023-07-16 12:11:56 -0600

    componentes iniciales

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { DbzListComponent } from './components/dbz-list/dbz-list.component';
+import { DbzFormComponent } from './components/dbz-form/dbz-form.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    DbzListComponent,
+    DbzFormComponent
   ],
   exports: [
     MainPageComponent
