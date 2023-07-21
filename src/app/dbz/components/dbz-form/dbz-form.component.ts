30be0e7cdabfaa1e8cfba5f9b5c9552b77931995
luis.villar <luis.villar@next-technologies.com.mx>
2023-07-21 17:46:09 -0600

    @Output()

--- src/app/dbz/components/dbz-form/dbz-form.component.ts
+++ src/app/dbz/components/dbz-form/dbz-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -7,6 +7,19 @@ import { Character } from '../../interfaces/character.interface';
   styleUrls: ['./dbz-form.component.scss']
 })
 export class DbzFormComponent {
+
+  @Output()
+  public justEmit: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  public myUser: EventEmitter<User> = new EventEmitter();
+
+  @Output()
+  public theEmitString: EventEmitter<string> = new EventEmitter();
+
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
   public character: Character = {
     name: '',
     power: 0
@@ -14,5 +27,32 @@ export class DbzFormComponent {
 
   public emitCharacterFn(): void {
     console.log('emit', this.character);
+
+    if (this.character.name.length === 0) return;
+
+    this.onNewCharacter.emit(this.character);
+    this.theEmitString.emit('Luis Alberto');
+
+    this.character.name  = '';
+    this.character.power = 0;
   };
+
+  public emitFn(): void {
+    this.myUser.emit({
+      id: 1,
+      name: 'Luis Alberto Villar Ortiz',
+      country: 'Mex'
+    });
+  };
+
+  public testEmit(): void {
+    this.justEmit.emit(15);
+  };
+};
+
+
+export interface User {
+  id: number;
+  name: string;
+  country: string;
 };
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,15 +1,32 @@
 import { Component } from "@angular/core";
 import { Character } from "../interfaces/character.interface";
+import { User } from "../components/dbz-form/dbz-form.component";
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
+  public num : number = 1;
   public characters: Character[] = [
     { name: 'Krillin', power: 1000 },
     { name: 'Goku', power: 9500 },
     { name: 'Vegeta', power: 7500 }
   ];
 
+  public onNewCharacterFather(character: Character): void {
+    console.log('ch main page...', character);
+  };
+
+  public stringFn(str: string): void {
+    console.log('str main page...', str);
+  };
+
+  public runsEmitFn(theUser: User): void {
+    console.log('theUser main page',  { ...theUser, id: this.num++ } );
+  }
+
+  public fatherFnString(theStr: any): void {
+    console.log('check str', theStr);
+  };
 
 };
